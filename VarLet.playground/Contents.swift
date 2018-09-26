18a00dffa7400e6be088af8069ec4a7e90f3c70d
Halil Özel <halilozel1903@gmail.com>
2018-09-26 18:49:44 +0300

    Değişkenler dersi eklendi.

---
+++ VarLet.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var deneme = 123 // sabit değerleri
+
+str = "Hello Swift"
+
+
+deneme = 33
+
+
+// var : değiştirilebilir değerlere sahiptir.
+
+// let : constant sabit değerlerdir daha sonradan değiştirilemezler.
+
+
+var string : String = "Halil"
+
+let sayi = 12.34
